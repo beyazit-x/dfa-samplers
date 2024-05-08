45622eba2c6a7c214f567b5ea788687bb3110f4d
Marcell Vazquez-Chanlatte <mvc@linux.com>
2024-05-07 19:56:28 -0700

    test: Check for non-emptiness.

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -1,5 +1,7 @@
 from dfa_sampler import gen_mutated_sequential_reach_avoid
 
 
-def test_smoke():
-    next(gen_mutated_sequential_reach_avoid())
+def test_non_empty():
+    for _ in range(100):
+        lang = next(gen_mutated_sequential_reach_avoid())
+        assert lang.find_word() is not None
